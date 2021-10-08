7af491fc18627687d333c82c72d2b6396e6f0154
Navindu <navanavi3@gmail.com>
2021-10-08 16:49:50 +0530

    add: tailwind classes for navbar and blocks

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,17 @@
-import { Typography } from 'antd';
+import { Typography } from "antd";
 
 const { Title } = Typography;
 
 const Navbar = () => {
-    return (
-        <div className="nav">
-            <div className="title-margin">
-                <Title level={2} className="marginless-t">Mage Hithe Videos</Title>
-            </div> 
-        </div>
-    )
-}
+  return (
+    <div className="bg-green-300 h-24 flex items-center pl-8">
+      <div className="title-margin">
+        <Title level={2} className="marginless-t">
+          Mage Hithe Videos
+        </Title>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import '../styles/globals.css'
-import 'antd/dist/antd.css';
+//import "../styles/globals.css";
+import "antd/dist/antd.css";
 
-import "../styles/componentStyles/Navbar.css"
-import '../styles/componentStyles/Blocks.css'
+// import "../styles/componentStyles/Navbar.css";
+// import "../styles/componentStyles/Blocks.css";
+
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
